32b1689669f2b079e471157cb47c3fcd0946da40
neopragma <davenicolette@gmail.com>
2023-11-14 08:37:55 -0700

    Implemented RPN calculator

--- tdd-rpn/src/main/java/com/neopragma/rpn/RPN.java
+++ tdd-rpn/src/main/java/com/neopragma/rpn/RPN.java
@@ -10,7 +10,42 @@ public class RPN
     }
 
     public String enter(String token) {
+        if (!isOperator(token) && stack.size() > 1) {
+            throw new RuntimeException("Please enter an operator");
+        }
+        if (isOperator(token)) {
+            if (stack.size() == 2) {
+                switch(token) {
+                    case "+":
+                        stack.push(String.valueOf(Double.parseDouble(stack.pop())
+                                + Double.parseDouble(stack.pop())));
+                        break;
+                    case "-":
+                        stack.push(String.valueOf(Double.parseDouble(stack.pop())
+                                - Double.parseDouble(stack.pop())));
+                        break;
+                    case "*":
+                        stack.push(String.valueOf(Double.parseDouble(stack.pop())
+                                * Double.parseDouble(stack.pop())));
+                        break;
+                    case "/":
+                        stack.push(String.valueOf(Double.parseDouble(stack.pop())
+                                / Double.parseDouble(stack.pop())));
+                        break;
+                }
+                 return stack.peek();
+            } else {
+                throw new RuntimeException("Please enter another number");
+            }
+        }
         stack.push(token);
         return stack.peek();
+    }
+
+    private boolean isOperator(String token) {
+        return token.equals("+") ||
+                token.equals("-") ||
+                token.equals("*") ||
+                token.equals("/");
     }
 }
--- tdd-rpn/src/test/java/com/neopragma/rpn/RPNTest.java
+++ tdd-rpn/src/test/java/com/neopragma/rpn/RPNTest.java
@@ -2,8 +2,14 @@ package com.neopragma.rpn;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RPNTest
 {
@@ -19,10 +25,48 @@ public class RPNTest
         assertEquals("3.75", rpn.enter("3.75"));
     }
 
-    @Test
-    public void entering_the_addition_operator_places_the_result_at_the_top_of_the_stack() {
-        rpn.enter("5.0");
-        rpn.enter("6.5");
-        assertEquals("11.5", rpn.enter("+"));
+    @ParameterizedTest
+    @MethodSource("provideValuesForRPN")
+    public void entering_an_operator_places_the_result_at_the_top_of_the_stack(
+            String value1, String value2, String operator, String expected) {
+        rpn.enter(value1);
+        rpn.enter(value2);
+        assertEquals(expected, rpn.enter(operator));
     }
+
+    private static Stream<Arguments> provideValuesForRPN() {
+        return Stream.of(
+                Arguments.of("5.5", "7.2", "+", "12.7"),
+                Arguments.of("3.5", "15.0", "-", "11.5"),
+                Arguments.of("2", "5", "*", "10.0"),
+                Arguments.of("2", "10", "/", "5.0")
+        );
+    }
+
+    @Test
+    public void it_handles_a_complex_expression() {
+        rpn.enter("5");
+        rpn.enter("4");
+        rpn.enter("+");
+        rpn.enter("2");
+        assertEquals("18.0", rpn.enter("*"));
+    }
+    @Test
+    public void when_there_are_too_few_operands_it_throws_an_exception() {
+        Exception exception = assertThrows(RuntimeException.class, () -> {
+            rpn.enter("6.5");
+            rpn.enter("+");
+        });
+        assertEquals("Please enter another number", exception.getMessage());
+    }
+    @Test
+    public void when_there_are_too_many_operands_it_throws_an_exception() {
+        Exception exception = assertThrows(RuntimeException.class, () -> {
+            rpn.enter("6.5");
+            rpn.enter("5.0");
+            rpn.enter("4.0");
+        });
+        assertEquals("Please enter an operator", exception.getMessage());
+    }
+
 }
