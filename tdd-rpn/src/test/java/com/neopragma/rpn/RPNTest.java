aa127878ed6d114d267ebfaa1743b51f8f527782
neopragma <davenicolette@gmail.com>
2023-11-14 06:44:13 -0700

    Added RPN starter code

---
+++ tdd-rpn/src/test/java/com/neopragma/rpn/RPNTest.java
@@ -0,0 +1,28 @@
+package com.neopragma.rpn;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RPNTest
+{
+    private RPN rpn;
+
+    @BeforeEach
+    public void beforeEachTestCase() {
+        rpn = new RPN();
+    }
+
+    @Test
+    public void entering_a_number_adds_the_number_to_the_stack() {
+        assertEquals("3.75", rpn.enter("3.75"));
+    }
+
+    @Test
+    public void entering_the_addition_operator_places_the_result_at_the_top_of_the_stack() {
+        rpn.enter("5.0");
+        rpn.enter("6.5");
+        assertEquals("11.5", rpn.enter("+"));
+    }
+}
